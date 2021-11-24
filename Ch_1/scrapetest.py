2516db5efa2af318392306e260c4dfff5d1bc0cd
@ynakimov <akim.off@yandex.ru>
2021-11-24 21:33:48 +0300

    1

--- Ch_1/scrapetest.py
+++ Ch_1/scrapetest.py
@@ -1,4 +1,12 @@
-from urllib.request import urlopen
+# from urllib.request import urlopen
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+# html = urlopen('http://pythonscraping.com/pages/page1.html')
+# print(html.read())
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+print(bs.h1)
