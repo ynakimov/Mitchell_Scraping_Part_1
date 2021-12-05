4757c5db91ca54e8ef24975479df0c9ec7038fae
@ynakimov <akim.off@yandex.ru>
2021-12-05 11:28:06 +0300

    Глава 3

--- Ch_2/scrapetest_2.py
+++ Ch_2/scrapetest_2.py
@@ -82,15 +82,15 @@ HEADERS_SBER_BANK = {
 #         print(link.attrs['href'])
 
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import datetime
-import random
-import re
-import requests
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+# import datetime
+# import random
+# import re
+# import requests
 
 
-random.seed(datetime.datetime.now())
+# random.seed(datetime.datetime.now())
 
 HEADERS_WIKI = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -98,15 +98,71 @@ HEADERS_WIKI = {
 }
 
 
-def getLinks(articleUrl):
-    # html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
-    us_url = 'http://en.wikipedia.org{}'.format(articleUrl)
-    html = requests.get(us_url, headers=HEADERS_WIKI)
-    bs = BeautifulSoup(html.text, 'html.parser')
-    return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+# def getLinks(articleUrl):
+#     # html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+#     us_url = 'http://en.wikipedia.org{}'.format(articleUrl)
+#     html = requests.get(us_url, headers=HEADERS_WIKI)
+#     bs = BeautifulSoup(html.text, 'html.parser')
+#     return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
 
-links = getLinks('/wiki/Kevin_Bacon')
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
-    print(newArticle)
-    links = getLinks(newArticle)
+# links = getLinks('/wiki/Kevin_Bacon')
+# while len(links) > 0:
+#     newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+#     print(newArticle)
+#     links = getLinks(newArticle)
+
+
+# from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import requests
+
+def get_html(pageUrl):
+    full_url = 'http://en.wikipedia.org{}'.format(pageUrl)
+    get_result = requests.get(full_url, headers=HEADERS_WIKI)
+    html = get_result.text
+    return html
+
+pages = set()
+# def getLinks(pageUrl):
+#     global pages
+#     # html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+    
+#     html = get_html(pageUrl)
+   
+#     bs = BeautifulSoup(html, 'html.parser')
+
+#     for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+#         if 'href' in link.attrs:
+#             if link.attrs['href'] not in pages:
+#                 #We have encountered a new page
+#                 newPage = link.attrs['href']
+#                 print(newPage)
+#                 pages.add(newPage)
+#                 getLinks(newPage)
+
+
+def getLinks(pageUrl):
+    # global pages
+    # html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+    html = get_html(pageUrl)
+    bs = BeautifulSoup(html, 'html.parser')
+    try:
+        print(bs.h1.get_text())
+        print(bs.find(id ='mw-content-text').find_all('p')[0].get_text())
+        print(bs.find(id='ca-edit').find('span').find('a').attrs['href'])
+    except AttributeError:
+        print('This page is missing something! Continuing.')
+    
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                #We have encountered a new page
+                newPage = link.attrs['href']
+                print('-'*20)
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+
+getLinks('')    
---
+++ Ch_3/scrapetest_3.py
@@ -0,0 +1,75 @@
+
+# from urllib.request import urlopen
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+
+import requests
+
+pages = set()
+random.seed(datetime.datetime.now())
+
+HEADERS_SBER_BANK = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+}
+
+HEADERS_WIKI = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+}
+
+
+def get_html(pageUrl, allow_redirects=True):
+    full_url = 'http://en.wikipedia.org{}'.format(pageUrl)
+    get_result = requests.get(full_url, headers=HEADERS_WIKI, allow_redirects=allow_redirects)
+    html = get_result.text
+    return html
+
+
+def getInternalLinks(bs, includeUrl):
+    includeUrl = '{}://{}'.format(urlparse(includeUrl).scheme, urlparse(includeUrl).netloc)
+    internalLinks = []
+    #Finds all links that begin with a "/"
+    for link in bs.find_all('a', href=re.compile('^(/|.*'+includeUrl+')')):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                if(link.attrs['href'].startswith('/')):
+                    internalLinks.append(includeUrl+link.attrs['href'])
+                else:
+                    internalLinks.append(link.attrs['href'])
+    return internalLinks
+            
+#Retrieves a list of all external links found on a page
+def getExternalLinks(bs, excludeUrl):
+    externalLinks = []
+    #Finds all links that start with "http" that do
+    #not contain the current URL
+    for link in bs.find_all('a', href=re.compile('^(http|www)((?!'+excludeUrl+').)*$')):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+def getRandomExternalLink(startingPage):
+    # html = urlopen(startingPage)
+    html = get_html(pageUrl)    
+    bs = BeautifulSoup(html, 'html.parser')
+    externalLinks = getExternalLinks(bs, urlparse(startingPage).netloc)
+    if len(externalLinks) == 0:
+        print('No external links, looking around the site for one')
+        domain = '{}://{}'.format(urlparse(startingPage).scheme, urlparse(startingPage).netloc)
+        internalLinks = getInternalLinks(bs, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0,
+                                    len(internalLinks)-1)])
+    else:
+        return externalLinks[random.randint(0, len(externalLinks)-1)]
+    
+def followExternalOnly(startingSite):
+    externalLink = getRandomExternalLink(startingSite)
+    print('Random external link is: {}'.format(externalLink))
+    followExternalOnly(externalLink)
+            
+followExternalOnly('http://oreilly.com')
