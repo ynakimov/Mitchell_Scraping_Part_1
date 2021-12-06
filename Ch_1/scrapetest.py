68fdb011fec4c428710d576f726ed8079ae250b7
@ynakimov <akim.off@yandex.ru>
2021-12-06 23:13:38 +0300

    5555555555

--- Ch_1/scrapetest.py
+++ Ch_1/scrapetest.py
@@ -3,6 +3,7 @@
 # html = urlopen('http://pythonscraping.com/pages/page1.html')
 # print(html.read())
 
+# date в timestamp в python
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -12,3 +13,12 @@ html = urlopen('http://www.pythonscraping.com/pages/page1.html')
 # bs = BeautifulSoup(html, 'html.parser')
 bs = BeautifulSoup(html, 'lxml')
 print(bs.h1)
+
+# date в timestamp в python
+import time
+import datetime
+s = "02/08/2021"
+s_timestamp = time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())
+print(s_timestamp)
+g = '1638219600000'
+h = '1627851600000'
--- Ch_3/scrapetest_3.py
+++ Ch_3/scrapetest_3.py
@@ -9,7 +9,7 @@ import random
 import requests
 
 pages = set()
-random.seed(datetime.datetime.now())
+random.seed(datetime.datetime.now().minute)
 
 HEADERS_SBER_BANK = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -24,9 +24,14 @@ HEADERS_ORELLY = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
 }
 
+last_external_link = 'http://oreilly.com'
+prelast_external_link = ''
 
 def get_html(startingPage, label_redirects=1):
-    
+    result = {
+        'html':'',
+        'status':0
+    }
     if label_redirects == 1:
         allow_redirects = True
     else:
@@ -34,21 +39,27 @@ def get_html(startingPage, label_redirects=1):
 
     # full_url = 'http://en.wikipedia.org{}'.format(pageUrl)
     get_result = requests.get(startingPage, headers=HEADERS_ORELLY, allow_redirects=allow_redirects)
+    result['status'] = get_result.status_code
     html = get_result.text
-    return html
+    result['html'] = html
+
+    return result
 
 
 def getInternalLinks(bs, includeUrl):
     includeUrl = '{}://{}'.format(urlparse(includeUrl).scheme, urlparse(includeUrl).netloc)
     internalLinks = []
+
     #Finds all links that begin with a "/"
     for link in bs.find_all('a', href=re.compile('^(/|.*'+includeUrl+')')):
         if link.attrs['href'] is not None:
             if link.attrs['href'] not in internalLinks:
                 if(link.attrs['href'].startswith('/')):
-                    internalLinks.append(includeUrl+link.attrs['href'])
+                    internalLink = includeUrl+link.attrs['href']
                 else:
-                    internalLinks.append(link.attrs['href'])
+                    internalLink = link.attrs['href']
+            if internalLink not in internalLinks:
+                internalLinks.append(internalLink)
     return internalLinks
 
 
@@ -57,7 +68,7 @@ def getExternalLinks(bs, excludeUrl):
     externalLinks = []
     #Finds all links that start with "http" that do
     #not contain the current URL
-    for link in bs.find_all('a', href=re.compile('^(http|www)((?!'+excludeUrl+').)*$')):
+    for link in bs.find_all('a', href=re.compile('^(http|www)((?!'+excludeUrl+').)+(?!(\.pdf|\.xls|\.doc))$')):
         if link.attrs['href'] is not None:
             if link.attrs['href'] not in externalLinks:
                 externalLinks.append(link.attrs['href'])
@@ -66,17 +77,32 @@ def getExternalLinks(bs, excludeUrl):
 
 def getRandomExternalLink(startingPage):
     # html = urlopen(startingPage)
-    html = get_html(startingPage)    
+    
+    global last_external_link
+
+    result = get_html(startingPage)
+    if result['status'] == 200:
+        html = result['html']
+    else:
+       html = get_html(last_external_link)
+
     bs = BeautifulSoup(html, 'html.parser')
     externalLinks = getExternalLinks(bs, urlparse(startingPage).netloc)
+
     if len(externalLinks) == 0:
         print('No external links, looking around the site for one')
         domain = '{}://{}'.format(urlparse(startingPage).scheme, urlparse(startingPage).netloc)
         internalLinks = getInternalLinks(bs, domain)
-        return getRandomExternalLink(internalLinks[random.randint(0,
-                                    len(internalLinks)-1)])
+        if len(internalLinks) == 0:
+            return getRandomExternalLink(last_external_link)
+            
+        else:
+            return getRandomExternalLink(internalLinks[random.randint(0,
+                                    len(internalLinks)-1)]) 
     else:
-        return externalLinks[random.randint(0, len(externalLinks)-1)]
+        last_external_link = startingPage
+        externalLink = externalLinks[random.randint(0, len(externalLinks)-1)]
+        return externalLink
 
 
 def followExternalOnly(startingSite):
@@ -85,4 +111,4 @@ def followExternalOnly(startingSite):
     followExternalOnly(externalLink)
 
             
-followExternalOnly('http://oreilly.com')
+followExternalOnly(last_external_link)
